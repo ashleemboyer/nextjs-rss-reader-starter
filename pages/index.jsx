d35c2d3e078ad522ecef2f19a03880e168cafc06
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-05-24 15:22:07 -0400

    Rendering list of articles

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,10 +2,32 @@
 // can read more about Pages in the Next.js docs at:
 // https://nextjs.org/docs/basic-features/pages
 
-const HomePage = () => (
+import { getAllArticles } from '@lib/articles';
+
+const HomePage = ({ articles }) => (
   <div>
-    <h1>Hello from HomePage!</h1>
+    <h1>My RSS Reader</h1>
+    {articles.map((article) => (
+      <div>
+        <h2>{article.title}</h2>
+        <p>{article.feedTitle}</p>
+        <p>{article.author}</p>
+        <p>{article.pubDate}</p>
+      </div>
+    ))}
   </div>
 );
+
+// Fetch all the articles in the feed once per day
+export async function getStaticProps() {
+  const articles = await getAllArticles();
+
+  return {
+    props: {
+      articles,
+    },
+    revalidate: 86400,
+  };
+}
 
 export default HomePage;
