aa4c4d1a404552bd9ae2b3e438a4f466de706832
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-05-24 12:57:26 -0400

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+// This component represents the index page for the site. You
+// can read more about Pages in the Next.js docs at:
+// https://nextjs.org/docs/basic-features/pages
+
+const HomePage = () => (
+  <div>
+    <h1>Hello from HomePage!</h1>
+  </div>
+);
+
+export default HomePage;
