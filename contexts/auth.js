7f1c15e30eb1c03b4bbe696bcaf4ace564edc1dc
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-05-25 11:44:58 -0400

    Removing router work from AuthProvider

--- contexts/auth.js
+++ contexts/auth.js
@@ -1,34 +1,17 @@
 import { createContext, useState, useEffect, useContext } from 'react';
-import { useRouter } from 'next/router';
 import { onAuthStateChanged } from '@lib/firebase';
 
-const DEFAULT_AUTH_DATA = { user: null, userLoading: null };
+const DEFAULT_AUTH_DATA = { user: null, userIsLoading: true };
 
 const AuthContext = createContext(DEFAULT_AUTH_DATA);
 
 export const AuthProvider = ({ children }) => {
-  const router = useRouter();
-  const isErrorPage = router.pathname.endsWith('error');
-  const isLoginPage = router.pathname.endsWith('login');
   const [authData, setAuthData] = useState(DEFAULT_AUTH_DATA);
 
   useEffect(() => {
-    setAuthData({ ...authData, userLoading: true });
+    setAuthData({ ...authData, userIsLoading: true });
     return onAuthStateChanged((user) => {
-      console.log('onAuthStateChanged', user);
-      if (isErrorPage) {
-        return;
-      }
-
-      if (!user && !isLoginPage) {
-        router.replace('/login');
-      }
-
-      if (user && isLoginPage) {
-        router.replace('/');
-      }
-
-      setAuthData({ user, userLoading: false });
+      setAuthData({ user, userIsLoading: false });
     });
   }, []);
 
